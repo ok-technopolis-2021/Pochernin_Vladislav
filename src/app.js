e81183fbf55d4a4268b593f507f9c37fdbf76773
vspochernin <vspochernin@gmail.com>
2021-10-27 00:00:24 +0300

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
-import './styles/style.scss'
-import 'images/github-logo.png'
+// Если будет нужен импорт такого рода, надо будет поставить rollup scss plugin.
+// В остальных случаях все стили подтянет gulp, просто по маске src/styles/**/*.less
+// и скомпилирует их в единый css файл
+
+// import './styles/style.scss'
+
+alert('Javascript Works!');
