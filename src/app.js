243ecb5d611cbc7c481859286208579d29ce35a3
Vladislav Pochernin <vspochernin@gmail.com>
2021-11-03 16:03:54 +0300

    move changeTheme() to .js file

--- src/app.js
+++ src/app.js
@@ -5,3 +5,15 @@
 // import './styles/style.scss'
 
 // alert('Javascript Works!');
+
+function changeTheme() {
+    if (document.body.classList.contains("dark-theme")) {
+        document.body.classList.replace("dark-theme", "light-theme");
+    } else {
+        document.body.classList.replace("light-theme", "dark-theme");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.body.addEventListener('click', changeTheme);
+});
